6a1c2c206298e475e19c9f2bb237a7827c0cb858
boxsteerr-pixel <boxsteerr-pixel@users.noreply.github.com>
2026-09-13 18:15:44 +0800

    Highlight wrong and standard answers in review

--- js/version.js
+++ js/version.js
@@ -1,2 +1,2 @@
 /* Single source of truth for release identifiers. Loaded before every app module and by service-worker.js. */
-globalThis.MATH_PWA_VERSION=Object.freeze({appVersion:'0.4.0',dbVersion:3,questionBankVersion:'2026.09.1',cacheVersion:'grade7-math-v17'});
+globalThis.MATH_PWA_VERSION=Object.freeze({appVersion:'0.4.0',dbVersion:3,questionBankVersion:'2026.09.1',cacheVersion:'grade7-math-v18'});
